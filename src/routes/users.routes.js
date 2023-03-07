4a6f667969dce9cd64bdccf8174a151454112753
Andres Felipe Villegas Caballero <felipevillegas81@gmail.com>
2023-03-07 14:48:13 -0500

    Segunda Practica Integrdara (Complementario)

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,4 +4,38 @@ import { hashPassword } from "../utils.js"
 
 const router = Router()
 
-router.get('/')
+router.get('/start', async (req, res) => {
+    const users = await usersDao.getAll()
+})
+
+router.get('/productid/:id', async (req, res) => {
+    const user = await usersDao.findById(req.params.id)
+    res.json(user)
+})
+
+router.post('/start', async (req, res) => {
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+    res.statusCode(400).json({ message: 'All fields are required'})
+    return
+    }
+
+    try {
+        const user = {
+            username,
+            email,
+            password: hashPassword(password)
+        }
+
+        const newUser = await usersDao.create(user)
+        res.json( {info: 'user Created', newUser})
+    } catch (error) {
+        res.statusCode(400).json({ message: error.message })
+    }
+
+
+})
+
+
+
